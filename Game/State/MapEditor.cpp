4e485a5b8933058c805383f9e6e3d703ee95934c
unknown <ledong2512@gmail.com>
2020-08-31 23:12:35 +0700

    +mapeditor coppy

--- Game/State/MapEditor.cpp
+++ Game/State/MapEditor.cpp
@@ -6,6 +6,7 @@
 #include "../ResourcesManager2D.h"
 #include "../SceneManager2D.h"
 #include "../Obstacle.h"
+#include "../SuicideBug.h"
 void getInfor() {
 	
 }
@@ -43,6 +44,7 @@ void MapEditor::Render()
 void MapEditor::Update(float deltaTime)
 {
 	
+	
 	if (m_curent != NULL) {
 		addObject();
 		m_curent = NULL;
@@ -60,7 +62,9 @@ void MapEditor::Update(float deltaTime)
 	camera.Dutch(Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::E) - Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::Q), deltaTime);
 	camera.Zoom(Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::X) - Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::Z), deltaTime);
 	camera.SetPosition(cameraPos);
-	if (m_currentSprite!=NULL&& Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_MOVE) {
+
+	int checkChoose = Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::LSHIFT);
+	if (m_currentSprite!=NULL&& (Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_MOVE|| Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_RELEASE)&& !checkChoose) {
 		int iMousePosX, iMousePosY, pre_x, pre_y;
 		Singleton<InputManager>::GetInstance()->getXY(iMousePosX, iMousePosY);
 		Vector3 pos3D = Singleton<SceneManager2D>::GetInstance()->get3Dpos(iMousePosX, iMousePosY, MAP_OBJECT);
@@ -68,22 +72,47 @@ void MapEditor::Update(float deltaTime)
 		Pos.x = pos3D.x;
 		Pos.y = pos3D.y;
 		m_currentSprite->SetPosition(Pos);
+		for (int i = 0;i < m_activeList.size();i++) {
+			Vector3 tmpPos= m_activeList[i]->GetPosition();
+			tmpPos.x = Pos.x + m_distance_x[i];
+			tmpPos.y = Pos.y + m_distance_y[i];
+			m_activeList[i]->SetPosition(tmpPos);
+		}
 		if (Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::SPACE)) {
-			for (int i = 0;i < m_Obstacle.size();i++) {
-				if (m_Obstacle[i] == m_currentSprite) {
-					m_Obstacle.erase(m_Obstacle.begin() + i);
-					break;
+			deleteSprite(m_currentSprite);
+			if (!m_activeList.empty()) {
+				for (int i = 0;i < m_activeList.size();i++) {
+					deleteSprite(m_activeList[i]);
 				}
 			}
-			for (int i = 0;i < m_suicudeBug.size();i++) {
-				if (m_suicudeBug[i] == m_currentSprite) {
-					m_suicudeBug.erase(m_suicudeBug.begin() + i);
-					break;
-				}
-			}
-			Singleton<SceneManager2D>::GetInstance()->RemoveMapObject(m_currentSprite);
-			delete m_currentSprite;
+			m_activeList.clear();
+			m_distance_x.clear();
+			m_distance_y.clear();
 			m_currentSprite = NULL;
+		}
+		else if (Singleton<InputManager>::GetInstance()->GetBit(InputManager::Key::DOWN)) {
+			Sprite* tmp=cloneSprite( m_currentSprite);
+			m_currentSprite->SetColor(m_norColor, 1);
+			m_currentSprite = tmp;
+			Singleton<SceneManager2D>::GetInstance()->AddObject(tmp, MAP_OBJECT);
+			pushSprite(tmp);
+			if (!m_activeList.empty()) {
+				std::vector<Sprite*> v_tmp;
+				for (int i = 0;i < m_activeList.size();i++) {
+					Sprite* tmp=cloneSprite( m_activeList[i]);
+					m_activeList[i]->SetColor(m_norColor, 1);;
+					Singleton<SceneManager2D>::GetInstance()->AddObject(tmp, MAP_OBJECT);
+					pushSprite(tmp);
+					v_tmp.push_back(tmp);
+				}
+				m_activeList.clear();
+				for (int i = 0;i < v_tmp.size();i++) {
+					m_activeList.push_back(v_tmp[i]);
+				}
+			}
+			Singleton<InputManager>::GetInstance()->KeyPressed(VK_DOWN, false);
+			
+
 		}
 		//Singleton<InputManager>::GetInstance()->fixButton();
 		
@@ -93,10 +122,36 @@ void MapEditor::Update(float deltaTime)
 		Singleton<InputManager>::GetInstance()->getXY(iMousePosX, iMousePosY);
 		Vector3 pos3D = Singleton<SceneManager2D>::GetInstance()->get3Dpos(iMousePosX, iMousePosY, MAP_OBJECT);
 		m_currentSprite = checkInside(pos3D.x, pos3D.y);
+		if(m_currentSprite) m_currentSprite->SetColor(m_chooseColor,1);
 		Singleton<InputManager>::GetInstance()->fixButton();
 	}
-	else if (Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_CLICK&& m_currentSprite != NULL) {
+	else if (Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_CLICK&& m_currentSprite != NULL&&!checkChoose) {
+		m_currentSprite->SetColor(m_norColor, 1);
 		m_currentSprite = NULL; Singleton<InputManager>::GetInstance()->fixButton();
+		
+		for (int i = 0;i < m_activeList.size();i++) m_activeList[i]->SetColor(m_norColor, 1);
+		m_distance_x.clear();m_distance_y.clear();
+		m_activeList.clear();
+	}
+	else if (Singleton<InputManager>::GetInstance()->getMouseEvent() == MOUSE_CLICK && m_currentSprite != NULL && checkChoose) {
+		int iMousePosX, iMousePosY, pre_x, pre_y;
+		Singleton<InputManager>::GetInstance()->getXY(iMousePosX, iMousePosY);
+		Vector3 pos3D = Singleton<SceneManager2D>::GetInstance()->get3Dpos(iMousePosX, iMousePosY, MAP_OBJECT);
+		Sprite* chooseSprite = checkInside(pos3D.x, pos3D.y),*tmpSprite;
+		
+		if (chooseSprite) {	
+			m_activeList.push_back(m_currentSprite);
+			m_currentSprite = chooseSprite;
+			m_currentSprite->SetColor(m_chooseColor, 1);
+			Vector2 distace(chooseSprite->GetPosition().x - m_currentSprite->GetPosition().x, chooseSprite->GetPosition().y - m_currentSprite->GetPosition().y);
+			m_distance_x.push_back(distace.x);m_distance_y.push_back(distace.y);
+			for (int i = 0;i < m_activeList.size() ;i++) {
+				Vector2 distace(m_activeList[i]->GetPosition().x - m_currentSprite->GetPosition().x, m_activeList[i]->GetPosition().y - m_currentSprite->GetPosition().y);
+				m_distance_x[i] = distace.x;
+				m_distance_y[i] = distace.y;
+			}
+		}
+		Singleton<InputManager>::GetInstance()->fixButton();
 	}
 }
 
@@ -124,6 +179,16 @@ void MapEditor::addObject()
 	position.y = 3;
 	position.z = -0.9;
 	obs->Init(position, rotation, scale, uiHexColor, alpha, iMaterialId, iMainTexId);
+	if (m_currentSprite) m_currentSprite->SetColor(m_norColor, 1);
+	if (!m_activeList.empty()) {
+		for (int i = 0;i < m_activeList.size();i++) {
+			m_activeList[i]->SetColor(m_norColor, 1);
+		}
+		m_activeList.clear();
+		m_distance_x.clear();
+		m_distance_y.clear();
+	}
+	
 	m_currentSprite = obs;
 	Singleton<SceneManager2D>::GetInstance()->AddObject(obs, MAP_OBJECT);
 	
@@ -139,7 +204,7 @@ void MapEditor::addObject()
 		Vector2 scale;
 		unsigned int uiHexColor;
 		float alpha;
-		Obstacle* obs = new Obstacle(m_curent->id, 0);
+		SuicideBug* obs = new SuicideBug(m_curent->id);
 		iMaterialId = 0;
 		iMainTexId = m_curent->mainTex;
 		rotation = 0;
@@ -151,6 +216,16 @@ void MapEditor::addObject()
 		position.y = 3;
 		position.z = -0.9;
 		obs->Init(position, rotation, scale, uiHexColor, alpha, iMaterialId, iMainTexId);
+		if (m_currentSprite) m_currentSprite->SetColor(m_norColor, 1);
+		if (!m_activeList.empty()) {
+			for (int i = 0;i < m_activeList.size();i++) {
+				m_activeList[i]->SetColor(m_norColor, 1);
+			}
+			m_activeList.clear();
+			m_distance_x.clear();
+			m_distance_y.clear();
+		}
+
 		m_currentSprite = obs;
 		Singleton<SceneManager2D>::GetInstance()->AddObject(obs, MAP_OBJECT);
 
@@ -161,6 +236,24 @@ void MapEditor::addObject()
 
 void MapEditor::KeyPress()
 {
+}
+
+void MapEditor::deleteSprite(Sprite* pointer)
+{
+	for (int i = 0;i < m_Obstacle.size();i++) {
+		if (m_Obstacle[i] == pointer) {
+			m_Obstacle.erase(m_Obstacle.begin() + i);
+			break;
+		}
+	}
+	for (int i = 0;i < m_suicudeBug.size();i++) {
+		if (m_suicudeBug[i] == pointer) {
+			m_suicudeBug.erase(m_suicudeBug.begin() + i);
+			break;
+		}
+	}
+	Singleton<SceneManager2D>::GetInstance()->RemoveMapObject(pointer);
+	delete pointer;
 }
 
 Sprite* MapEditor::checkInside(float x, float y)
@@ -180,5 +273,28 @@ Sprite* MapEditor::checkInside(float x, float y)
 			return m_suicudeBug[i];
 	}
 	return nullptr;
+}
+
+void MapEditor::pushSprite(Sprite* pointer)
+{
+	if (dynamic_cast<Obstacle*>(pointer)) {
+		m_Obstacle.push_back(pointer);
+	}else if (dynamic_cast<SuicideBug*>(pointer)) {
+		m_suicudeBug.push_back(pointer);
+	}
+}
+
+Sprite* MapEditor::cloneSprite( Sprite* source)
+{
+	Sprite* destination=NULL;
+	if (dynamic_cast<Obstacle*>(source)) {
+		destination = new Obstacle(0, 0);
+		destination->Init(*source);
+	}
+	else if (dynamic_cast<SuicideBug*>(source)) {
+		destination = new SuicideBug(0);
+		destination->Init(*source);
+	}
+	return destination;
 }
 
--- Game/State/MapEditor.h
+++ Game/State/MapEditor.h
@@ -16,11 +16,19 @@ public:
 	void Update(float deltaTime);
 	void addObject();
 	void KeyPress();
+	void deleteSprite(Sprite* pointer);
 	Sprite* checkInside(float x, float y);
+	void pushSprite(Sprite* pointer);
+	Sprite* cloneSprite(Sprite* source);
 private:
 	std::vector<UnitButton*> button;
 	int m_mouseDragCheck=0;
+	int m_norColor = 0xffffff;
+	int m_chooseColor = 0xffafff;
+
 	std::vector<Sprite*> m_Obstacle,m_suicudeBug;
+	std::vector<Sprite*> m_activeList;
+	std::vector<float> m_distance_x, m_distance_y;
 	UnitInfor *m_curent ;
 	Sprite* m_currentSprite ;
 };
